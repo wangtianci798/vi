346a4fc359561b2e546e543b44bf5d9a3e9815a1
王天赐 <13405446960@163.com>
2025-05-29 20:31:01 +0800

    select/poll/epoll

--- multi-io.c
+++ multi-io.c
@@ -6,6 +6,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <sys/poll.h>
+#include <sys/epoll.h>
 
 void *client_thread(void *arg){
 
@@ -66,7 +68,7 @@ int main()
     }
 #endif
 
-#else
+#elif 0
 
     while(1){
 
@@ -78,7 +80,150 @@ int main()
     pthread_create(&thid, NULL, client_thread, &clientfd);
 
     }
+#elif 0 //select
 
+    //select(maxfd, rest, wset, eset, timeout)
+
+    fd_set rfds, rset;
+    FD_ZERO(&rfds);
+    FD_SET(sockfd, &rfds);
+
+    int maxfd = sockfd;
+
+    printf("loop\n");
+
+    while(1){
+
+        rset = rfds;
+
+        int nready = select(maxfd+1, &rset, NULL, NULL, NULL);
+
+        if(FD_ISSET(sockfd, &rset)){
+            
+            struct sockaddr_in clientaddr;
+            socklen_t len = sizeof(clientaddr);
+
+            int clientfd = accept(sockfd,(struct sockaddr*)&clientaddr, &len); 
+            
+            printf("sockfd: %d\n", clientfd);
+
+            FD_SET(clientfd, &rfds);
+
+            maxfd = clientfd;
+        }
+        int i = 0;
+        for(i = sockfd + 1; i<= maxfd; i++){
+
+            if(FD_ISSET(i, &rset)){
+
+            char buffer[128] = {0};
+            int count = recv(i, buffer, 128, 0);
+            if(count == 0){
+                FD_CLR(i, &rfds);
+                close(i);
+
+            break;
+            }
+            send(i, buffer, count, 0);
+            printf("clientfd: %d, count: %d, buffer: %s\n", i, count, buffer);
+            }
+        }
+    }
+#elif 0
+        struct pollfd  fds[1024] = {0};
+
+        fds[sockfd].fd = sockfd;
+        fds[sockfd].events = POLLIN;
+
+        int maxfd = sockfd;
+
+        while(1){
+
+            int nready = poll(fds, maxfd+1, -1);
+
+            if(fds[sockfd].revents & POLLIN){
+            struct sockaddr_in clientaddr;
+            socklen_t len = sizeof(clientaddr);
+
+            int clientfd = accept(sockfd,(struct sockaddr*)&clientaddr, &len); 
+            
+            printf("sockfd: %d\n", clientfd); 
+            fds[clientfd].fd = sockfd;
+            fds[clientfd].events = POLLIN;
+
+            maxfd = clientfd;
+            }
+
+            int i = 0;
+            for(i = sockfd + 1; i<= maxfd; i++){
+            
+            if(fds[i].revents & POLLIN){
+
+            char buffer[128] = {0};
+            int count = recv(i, buffer, 128, 0);
+            if(count == 0){
+           
+            fds[i].fd = -1;
+            fds[i].events = 0;
+
+            break;
+            }
+            send(i, buffer, count, 0);
+            printf("clientfd: %d, count: %d, buffer: %s\n", i, count, buffer);
+
+            }
+            }
+        }
+
+#else 
+
+    int epfd = epoll_create(1); //int size
+
+    struct epoll_event ev;
+    ev.events = EPOLLIN;
+    ev.data.fd = sockfd;
+
+    epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &ev);
+
+    struct epoll_event events[1024] = {0};
+    while(1){
+
+        int nready = epoll_wait(epfd, events, 1024, -1);
+        
+        int i = 0;
+        for(i = 0; i < nready; i++)
+        {
+            int connfd = events[i].data.fd;
+            if(sockfd == connfd){
+
+            struct sockaddr_in clientaddr;
+            socklen_t len = sizeof(clientaddr);
+
+            int clientfd = accept(sockfd,(struct sockaddr*)&clientaddr, &len); 
+
+            ev.events = EPOLLIN;
+            ev.data.fd = clientfd;
+            epoll_ctl(epfd, EPOLL_CTL_ADD, clientfd, &ev);
+
+            printf("clientfd: %d\n", clientfd);
+
+            }else if(events[i].events & EPOLLIN){
+
+            char buffer[128] = {0};
+            int count = recv(connfd, buffer, 128, 0);
+            if(count == 0){
+                printf("disconnect");
+
+                epoll_ctl(epfd, EPOLL_CTL_DEL, connfd, &ev);
+                continue;
+            }
+            send(connfd, buffer, count, 0);
+            printf("clientfd: %d, count: %d, buffer: %s\n", connfd, count, buffer);
+
+            }
+        }
+
+    }
 
 #endif
     
